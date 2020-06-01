fc3fa5d77328938dbd22646e4014890f2af3503f
Wes <wesyes@att.net>
2020-05-31 22:54:32 -0700

    Clean up files

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Table from './Table.js';
-import Reward from './Reward.js';
-import Categories from './Categories.js';
 
 function App() {
   return (
